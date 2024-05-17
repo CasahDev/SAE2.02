57b6cb50c49cfe2bcd1db4ff4e2716a4775bcbfe
SachaLaurent <sacha.laurent71@gmail.com>
2024-05-17 18:12:49 +0200

    Patch bug ou la recherche locale revenait en arrière et donnait un chemin plus long (calcul de distance)

--- VoyageurDeCommerce/modele/algorithmes/realisations/AlgorithmeRechercheLocale.cs
+++ VoyageurDeCommerce/modele/algorithmes/realisations/AlgorithmeRechercheLocale.cs
@@ -14,128 +14,81 @@ namespace VoyageurDeCommerce.modele.algorithmes.realisations
         private Stopwatch sw;
 
         public override string Nom => "Recherche locale";
-        
+
         public override void Executer(List<Lieu> listeLieux, List<Route> listeRoute)
         {
-            bool condition = false;
-            int compteur = 0;
-            
             sw = Stopwatch.StartNew();
             sw.Start();
-            
-            FloydWarshall.calculerDistances(listeLieux,listeRoute);
+
+            FloydWarshall.calculerDistances(listeLieux, listeRoute);
             Algorithme algo = new AlgorithmeCroissant();
-            algo.Executer(listeLieux,listeRoute);
+            algo.Executer(listeLieux, listeRoute);
 
 
             foreach (Lieu lieu in algo.Tournee.ListeLieux)
             {
-                this.Tournee.ListeLieux.Add(lieu);
+                Tournee.ListeLieux.Add(lieu);
                 sw.Stop();
                 NotifyPropertyChanged("Tournee");
                 sw.Start();
             }
 
-            while (condition == false && compteur < 10)
+            bool gotBetterSolution = true;
+            int maxAttempt = 0;
+            while (gotBetterSolution && maxAttempt < 10)
             {
-                condition = ChercherVoisine();
-                compteur++;
+                gotBetterSolution = ChercherVoisine();
+                maxAttempt++;
             }
+
             sw.Stop();
             TempsExecution = sw.ElapsedMilliseconds;
-
-            
         }
 
         private bool ChercherVoisine()
         {
-
-            List<Lieu> liste = Tournee.ListeLieux;
             Tournee tournee = new Tournee(Tournee);
-            
+
             for (int i = 0; i < Tournee.ListeLieux.Count; i++)
             {
-                TourneeMin(liste, i);
+                int min = Tournee.Distance;
+                int nvDistance = CalculeDistance(Tournee.ListeLieux, min, i);
+
+                if (nvDistance < min)
+                {
+                    Exchange(Tournee.ListeLieux, i);
+                }
             }
 
-            return tournee == Tournee;
+            return tournee != Tournee;
         }
 
         private int CalculeDistance(List<Lieu> listeLieux, int distance, int index)
         {
 
-            int result = 0;
-            
+            int result = distance +
+                         FloydWarshall.Distance(listeLieux[Mod(index + 1, listeLieux.Count)],
+                             listeLieux[Mod(index - 1, listeLieux.Count)]) +
+                         FloydWarshall.Distance(listeLieux[index], listeLieux[Mod(index + 2, listeLieux.Count)]) -
+                         FloydWarshall.Distance(listeLieux[Mod(index - 1, listeLieux.Count)], listeLieux[index]) -
+                         FloydWarshall.Distance(listeLieux[Mod(index + 1, listeLieux.Count)],
+                             listeLieux[Mod(index + 2, listeLieux.Count)]);
 
-            if ((index+1) == listeLieux.Count)
-            {
-                result = distance + FloydWarshall.Distance(listeLieux[0], listeLieux[index - 1]) +
-                         FloydWarshall.Distance(listeLieux[index], listeLieux[1]) -
-                         FloydWarshall.Distance(listeLieux[index], listeLieux[0]) -
-                         FloydWarshall.Distance(listeLieux[1], listeLieux[index-1]);
-            }
-            else if ((index+2) == listeLieux.Count)
-            {
-                result = distance + FloydWarshall.Distance(listeLieux[index + 1], listeLieux[index - 1]) +
-                         FloydWarshall.Distance(listeLieux[index], listeLieux[0]) -
-                         FloydWarshall.Distance(listeLieux[index], listeLieux[index + 1]) -
-                         FloydWarshall.Distance(listeLieux[0], listeLieux[index - 1]);
-            }
-            else if (index == 0)
-            {
-                result = distance + FloydWarshall.Distance(listeLieux[index + 1], listeLieux[listeLieux.Count-1]) +
-                         FloydWarshall.Distance(listeLieux[index], listeLieux[index + 2]) -
-                         FloydWarshall.Distance(listeLieux[index], listeLieux[index + 1]) -
-                         FloydWarshall.Distance(listeLieux[listeLieux.Count-1], listeLieux[index + 2]);
-            }
-            else
-            {
-                result = distance + FloydWarshall.Distance(listeLieux[index+1], listeLieux[index - 1]) +
-                         FloydWarshall.Distance(listeLieux[index], listeLieux[index + 2]) -
-                         FloydWarshall.Distance(listeLieux[index], listeLieux[index + 1]) -
-                         FloydWarshall.Distance(listeLieux[index-1], listeLieux[index + 2]);
-            }
             return result;
-        }
-
-        private void TourneeMin(List<Lieu> listeLieux, int index)
-        {
-
-            int minimum = Tournee.Distance;
-            int nvDistance = CalculeDistance(listeLieux, minimum, index);
-
-            if (nvDistance < minimum)
-            {
-                minimum = nvDistance;
-                Exchange(listeLieux, index);
-            }
-
-
         }
 
         private void Exchange(List<Lieu> listeLieux, int index)
         {
+            (listeLieux[index], listeLieux[Mod(index + 1, listeLieux.Count)]) =
+                (listeLieux[Mod((index + 1), listeLieux.Count)], listeLieux[index]);
 
-            Lieu temp;
-            if (index == listeLieux.Count-1)
-            {
-                temp = listeLieux[index];
-                listeLieux[index] = listeLieux[0];
-                listeLieux[0] = temp;
-            }
-            else
-            {
-                temp = listeLieux[index];
-                listeLieux[index] = listeLieux[index + 1];
-                listeLieux[index + 1] = temp;
-            }
-
-            
-            Tournee.ListeLieux = listeLieux;
             sw.Stop();
             NotifyPropertyChanged("Tournee");
             sw.Start();
         }
-        
+
+        private int Mod(int x, int m) {
+            return (x%m + m)%m;
+        }
     }
 }
